0c2d890beaa7a99844b115b50ec7bcd75d1799bc
phuochaufms71 <phuochaubmw@gmail.com>
2024-08-05 23:47:25 +0700

    fix adminCreateFood.jsx

--- src/pages/admin/AdminCreateFood/AdminCreateFood.jsx
+++ src/pages/admin/AdminCreateFood/AdminCreateFood.jsx
@@ -10,30 +10,40 @@ import { images } from "../../../components/images/index.js";
 const AdminCreateFood = () => {
   const cx = classNames.bind(styles);
   const dispatch = useDispatch();
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [rate, setRate] = useState("");
+  const [description, setDescription] = useState("");
   const [image, setImage] = useState(false);
-  const [formData, setFormData] = useState({
-    name: '',
-    category: '',
-    price: '',
-    image: image,
-    rate: '',
-    description: ''
-  })
+  // const [formData, setFormData] = useState({
+  //   name: '',
+  //   category: '',
+  //   price: '',
+  //   image: '',
+  //   rate: '',
+  //   description: ''
+  // })
+  const formData = new FormData();
+  formData.append("name", name)
+  formData.append("category", category)
+  formData.append("price", price)
+  formData.append("image", image)
+  formData.append("rate", rate)
+  formData.append("description", description)
   const handleSubmitFormCreateFood = async (e) => {
     e.preventDefault();
     const accessToken = localStorage.getItem(ACCESS_TOKEN);
     await dispatch(createFood({
       accessToken,
-      newFood: {...formData, image}
+      newFood: formData
     }))
     setImage(false)
-    setFormData({
-      name: '',
-      category: '',
-      price: '',
-      rate: '',
-      description: ''
-    })
+    setName("")
+    setCategory("")
+    setPrice("")
+    setRate("")
+    setDescription("")
   }
 
   return (
@@ -43,15 +53,15 @@ const AdminCreateFood = () => {
           <div className={cx("create-food__form-block")}>
             <div className={cx("create-food__form-group")}>
               <label className={cx("create-food__label")} htmlFor="nameFood">Name</label>
-              <input className={cx("create-food__input")} type="text" value={formData.name} onChange={(e) => setFormData({...formData, name: e.target.value})} id="nameFood"/>
+              <input className={cx("create-food__input")} type="text" value={name} onChange={(e) => setName(e.target.value)} id="nameFood"/>
             </div>
             <div className={cx("create-food__form-group")}>
               <label className={cx("create-food__label")} htmlFor="categoryFood">Category</label>
-              <input className={cx("create-food__input")} type="text" value={formData.category} onChange={(e) => setFormData({...formData, category: e.target.value})} id="categoryFood"/>
+              <input className={cx("create-food__input")} type="text" value={category} onChange={(e) => setCategory(e.target.value)} id="categoryFood"/>
             </div>
             <div className={cx("create-food__form-group")}>
               <label className={cx("create-food__label")} htmlFor="priceFood">Price</label>
-              <input className={cx("create-food__input")} type="text" value={formData.price} onChange={(e) => setFormData({...formData, price: e.target.value})} id="priceFood"/>
+              <input className={cx("create-food__input")} type="text" value={price} onChange={(e) => setPrice(e.target.value)} id="priceFood"/>
             </div>
           </div>
           <div className={cx("create-food__form-block")}>
@@ -64,13 +74,13 @@ const AdminCreateFood = () => {
             </div>
             <div className={cx("create-food__form-group")}>
               <label className={cx("create-food__label")} htmlFor="rateFood">Rate</label>
-              <input className={cx("create-food__input")} type="text" value={formData.rate} onChange={(e) => setFormData({...formData, rate: e.target.value})} id="rateFood"/>
+              <input className={cx("create-food__input")} type="text" value={rate} onChange={(e) => setRate(e.target.value)} id="rateFood"/>
             </div>
           </div>
           <div className={cx("create-food__form-block")}>
             <div className={cx("create-food__form-group")}>
               <label className={cx("create-food__label")} htmlFor="descFood">Description</label>
-              <textarea rows={6} className={cx("create-food__textarea")} value={formData.description} onChange={(e) => setFormData({...formData, description: e.target.value})} id="descFood"/>
+              <textarea rows={6} className={cx("create-food__textarea")} value={formData.description} onChange={(e) => setDescription(e.target.value)} id="descFood"/>
             </div>
           </div>
           <div className={cx("create-food__footer")}>
