f187f18c8763ecee0f1ba74b4d706713a4766fd6
phuochaufms71 <phuochaubmw@gmail.com>
2024-08-06 14:08:36 +0700

    fix bugs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import authReducer from './auth/authSlice';
-import foodReducer from './food/foodSlice'
+import foodReducer from './food/foodSlice';
 
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
